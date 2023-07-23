15b33501936cc95383797a3ebbe4a668fc114ab5
vincent-tarrit <vincent@tarrit.com>
2023-07-23 12:15:08 +0200

    try

--- src/Services/CampaignService.php
+++ src/Services/CampaignService.php
@@ -6,7 +6,7 @@ use Vincenttarrit\Newsletter\Models\Campaign;
 use Vincenttarrit\Newsletter\Models\Domain;
 
 class CampaignService {
-    public function make(?string $domainId) : Campaign
+    public function make(?string $domainId = null) : Campaign
     {
         $domain = Domain::from($domainId ?? config('infomaniak-newsletter.default_domain_id'));
         return new Campaign($domain);
